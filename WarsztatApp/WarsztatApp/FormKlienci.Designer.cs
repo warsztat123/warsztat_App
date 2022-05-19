b4bda85688aabb11e50069576889f90e1665ae92
Adam Dziakiewicz <adam.dziakiewicz@outlook.com>
2022-05-19 16:07:18 +0200

    dodawanie klienta do bazy danych

--- WarsztatApp/WarsztatApp/FormKlienci.Designer.cs
+++ WarsztatApp/WarsztatApp/FormKlienci.Designer.cs
@@ -35,16 +35,20 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.button4 = new System.Windows.Forms.Button();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtMiasto = new System.Windows.Forms.TextBox();
+            this.txtNumer = new System.Windows.Forms.TextBox();
+            this.txtUlica = new System.Windows.Forms.TextBox();
+            this.txtTelefon = new System.Windows.Forms.TextBox();
+            this.txtNazwisko = new System.Windows.Forms.TextBox();
+            this.txtImie = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtWojewodztwo = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtKodPocztowy = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -54,9 +58,9 @@
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label6.Location = new System.Drawing.Point(12, 292);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(64, 25);
+            this.label6.Size = new System.Drawing.Size(70, 25);
             this.label6.TabIndex = 44;
-            this.label6.Text = "label6";
+            this.label6.Text = "Miasto";
             // 
             // label5
             // 
@@ -64,9 +68,9 @@
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label5.Location = new System.Drawing.Point(12, 239);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(64, 25);
+            this.label5.Size = new System.Drawing.Size(70, 25);
             this.label5.TabIndex = 43;
-            this.label5.Text = "label5";
+            this.label5.Text = "Numer";
             // 
             // label4
             // 
@@ -74,9 +78,9 @@
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label4.Location = new System.Drawing.Point(12, 186);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(64, 25);
+            this.label4.Size = new System.Drawing.Size(55, 25);
             this.label4.TabIndex = 42;
-            this.label4.Text = "label4";
+            this.label4.Text = "Ulica";
             // 
             // label3
             // 
@@ -84,9 +88,9 @@
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label3.Location = new System.Drawing.Point(12, 131);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(64, 25);
+            this.label3.Size = new System.Drawing.Size(78, 25);
             this.label3.TabIndex = 41;
-            this.label3.Text = "label3";
+            this.label3.Text = "Telefon";
             // 
             // label2
             // 
@@ -94,9 +98,9 @@
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label2.Location = new System.Drawing.Point(12, 72);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(64, 25);
+            this.label2.Size = new System.Drawing.Size(96, 25);
             this.label2.TabIndex = 40;
-            this.label2.Text = "label2";
+            this.label2.Text = "Nazwisko";
             // 
             // label1
             // 
@@ -104,9 +108,10 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label1.Location = new System.Drawing.Point(12, 18);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(64, 25);
+            this.label1.Size = new System.Drawing.Size(48, 25);
             this.label1.TabIndex = 39;
-            this.label1.Text = "label1";
+            this.label1.Text = "Imie";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // button4
             // 
@@ -118,53 +123,53 @@
             this.button4.Text = "Wyświetl";
             this.button4.UseVisualStyleBackColor = true;
             // 
-            // textBox6
+            // txtMiasto
             // 
-            this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.textBox6.Location = new System.Drawing.Point(151, 289);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(212, 30);
-            this.textBox6.TabIndex = 37;
+            this.txtMiasto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtMiasto.Location = new System.Drawing.Point(151, 292);
+            this.txtMiasto.Name = "txtMiasto";
+            this.txtMiasto.Size = new System.Drawing.Size(212, 30);
+            this.txtMiasto.TabIndex = 37;
             // 
-            // textBox5
+            // txtNumer
             // 
-            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.textBox5.Location = new System.Drawing.Point(151, 236);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(212, 30);
-            this.textBox5.TabIndex = 36;
+            this.txtNumer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtNumer.Location = new System.Drawing.Point(151, 236);
+            this.txtNumer.Name = "txtNumer";
+            this.txtNumer.Size = new System.Drawing.Size(212, 30);
+            this.txtNumer.TabIndex = 36;
             // 
-            // textBox4
+            // txtUlica
             // 
-            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.textBox4.Location = new System.Drawing.Point(151, 183);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(212, 30);
-            this.textBox4.TabIndex = 35;
+            this.txtUlica.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtUlica.Location = new System.Drawing.Point(151, 183);
+            this.txtUlica.Name = "txtUlica";
+            this.txtUlica.Size = new System.Drawing.Size(212, 30);
+            this.txtUlica.TabIndex = 35;
             // 
-            // textBox3
+            // txtTelefon
             // 
-            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.textBox3.Location = new System.Drawing.Point(151, 128);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(212, 30);
-            this.textBox3.TabIndex = 34;
+            this.txtTelefon.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtTelefon.Location = new System.Drawing.Point(151, 128);
+            this.txtTelefon.Name = "txtTelefon";
+            this.txtTelefon.Size = new System.Drawing.Size(212, 30);
+            this.txtTelefon.TabIndex = 34;
             // 
-            // textBox2
+            // txtNazwisko
             // 
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.textBox2.Location = new System.Drawing.Point(151, 69);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(212, 30);
-            this.textBox2.TabIndex = 33;
+            this.txtNazwisko.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtNazwisko.Location = new System.Drawing.Point(151, 69);
+            this.txtNazwisko.Name = "txtNazwisko";
+            this.txtNazwisko.Size = new System.Drawing.Size(212, 30);
+            this.txtNazwisko.TabIndex = 33;
             // 
-            // textBox1
+            // txtImie
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.textBox1.Location = new System.Drawing.Point(151, 15);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(212, 30);
-            this.textBox1.TabIndex = 32;
+            this.txtImie.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtImie.Location = new System.Drawing.Point(151, 15);
+            this.txtImie.Name = "txtImie";
+            this.txtImie.Size = new System.Drawing.Size(212, 30);
+            this.txtImie.TabIndex = 32;
             // 
             // button1
             // 
@@ -195,6 +200,7 @@
             this.button3.TabIndex = 29;
             this.button3.Text = "Dodaj";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // dataGridView1
             // 
@@ -209,11 +215,51 @@
             this.dataGridView1.Size = new System.Drawing.Size(805, 725);
             this.dataGridView1.TabIndex = 28;
             // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label7.Location = new System.Drawing.Point(12, 345);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(134, 25);
+            this.label7.TabIndex = 46;
+            this.label7.Text = "Województwo";
+            // 
+            // txtWojewodztwo
+            // 
+            this.txtWojewodztwo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtWojewodztwo.Location = new System.Drawing.Point(151, 340);
+            this.txtWojewodztwo.Name = "txtWojewodztwo";
+            this.txtWojewodztwo.Size = new System.Drawing.Size(212, 30);
+            this.txtWojewodztwo.TabIndex = 45;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label8.Location = new System.Drawing.Point(7, 405);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(137, 25);
+            this.label8.TabIndex = 48;
+            this.label8.Text = "Kod Pocztowy";
+            // 
+            // txtKodPocztowy
+            // 
+            this.txtKodPocztowy.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtKodPocztowy.Location = new System.Drawing.Point(151, 400);
+            this.txtKodPocztowy.Name = "txtKodPocztowy";
+            this.txtKodPocztowy.Size = new System.Drawing.Size(212, 30);
+            this.txtKodPocztowy.TabIndex = 47;
+            // 
             // FormKlienci
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1200, 750);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtKodPocztowy);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtWojewodztwo);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -221,12 +267,12 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button4);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtMiasto);
+            this.Controls.Add(this.txtNumer);
+            this.Controls.Add(this.txtUlica);
+            this.Controls.Add(this.txtTelefon);
+            this.Controls.Add(this.txtNazwisko);
+            this.Controls.Add(this.txtImie);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button3);
@@ -250,15 +296,19 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtMiasto;
+        private System.Windows.Forms.TextBox txtNumer;
+        private System.Windows.Forms.TextBox txtUlica;
+        private System.Windows.Forms.TextBox txtTelefon;
+        private System.Windows.Forms.TextBox txtNazwisko;
+        private System.Windows.Forms.TextBox txtImie;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtWojewodztwo;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtKodPocztowy;
     }
 }
--- WarsztatApp/WarsztatApp/FormKlienci.cs
+++ WarsztatApp/WarsztatApp/FormKlienci.cs
@@ -7,14 +7,88 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace WarsztatApp
 {
     public partial class FormKlienci : Form
     {
+        SqlConnection sqlConnection = new SqlConnection();
+        SqlCommand sqlCommand;
+       
+
         public FormKlienci()
         {
             InitializeComponent();
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            sqlConnection.ConnectionString = "Data Source=LAPTOP-442E11CB\\SQLEXPRESS;"
+                                                + "Initial Catalog=Warsztat;"
+                                                + "Integrated Security=True";
+
+            try
+            {
+                sqlConnection.Open();
+              
+                string Query = "EXEC USP_WstawDaneKlienta @imie,@nazw,@tel,@ulica,@nrUlicy,@miasto,@wojew,@kodPocz";
+                
+               
+	
+                sqlCommand = new SqlCommand
+                    (Query, sqlConnection);
+
+                sqlCommand.Parameters.Add("@imie", SqlDbType.NVarChar);
+                sqlCommand.Parameters.Add("@nazw", SqlDbType.NVarChar);
+                sqlCommand.Parameters.Add("@tel", SqlDbType.NVarChar);
+                sqlCommand.Parameters.Add("@ulica", SqlDbType.NVarChar);
+                sqlCommand.Parameters.Add("@nrUlicy", SqlDbType.NVarChar);
+                sqlCommand.Parameters.Add("@miasto", SqlDbType.NVarChar);
+                sqlCommand.Parameters.Add("@wojew", SqlDbType.NVarChar);
+                sqlCommand.Parameters.Add("@kodPocz", SqlDbType.NVarChar);
+
+                sqlCommand.Parameters["@imie"].Value=txtImie.Text;
+                sqlCommand.Parameters["@nazw"].Value=txtNazwisko.Text;
+                sqlCommand.Parameters["@tel"].Value=txtTelefon.Text;
+                sqlCommand.Parameters["@ulica"].Value=txtUlica.Text;
+                sqlCommand.Parameters["@NrUlicy"].Value=txtNumer.Text;
+                sqlCommand.Parameters["@miasto"].Value=txtMiasto.Text;
+                sqlCommand.Parameters["@wojew"].Value=txtWojewodztwo.Text;
+                sqlCommand.Parameters["@kodPocz"].Value=txtKodPocztowy.Text;
+
+                /*sqlCommand.Parameters.AddWithValue("@nazw", txtNazwisko.Text);
+                sqlCommand.Parameters.AddWithValue("@tel", txtTelefon.Text);
+                sqlCommand.Parameters.AddWithValue("@ulica", txtUlica.Text);
+                sqlCommand.Parameters.AddWithValue("@nrUlicy", txtNumer.Text);
+                sqlCommand.Parameters.AddWithValue("@misto", txtMiasto.Text);
+                sqlCommand.Parameters.AddWithValue("@wojew", txtWojewodztwo.Text);
+                sqlCommand.Parameters.AddWithValue("@kodPocz", txtKodPocztowy.Text);*/
+                
+
+               
+                sqlCommand.ExecuteNonQuery();
+                MessageBox.Show("użytkownik dodany pomyślnie");
+            }
+            catch (SqlException)
+            {
+                MessageBox.Show("Błędne dane", "Bląd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                //Application.Restart();
+                //Environment.Exit(0);
+            }
+
+            finally
+            {
+                sqlConnection.Close();
+            }
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
+
