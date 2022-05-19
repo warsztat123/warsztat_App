1c402ba4fba21dbf206b70eafd2cf16a5dc193da
Adam Dziakiewicz <adam.dziakiewicz@outlook.com>
2022-05-19 22:01:33 +0200

    modyfikacja danych klienta

--- WarsztatApp/WarsztatApp/FormKlienci.Designer.cs
+++ WarsztatApp/WarsztatApp/FormKlienci.Designer.cs
@@ -192,6 +192,7 @@
             this.button2.TabIndex = 30;
             this.button2.Text = "Modyfikuj";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -214,9 +215,11 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
             this.dataGridView1.Size = new System.Drawing.Size(805, 725);
             this.dataGridView1.TabIndex = 28;
             this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_RowHeaderMouseClick);
             // 
             // label7
             // 
--- WarsztatApp/WarsztatApp/FormKlienci.cs
+++ WarsztatApp/WarsztatApp/FormKlienci.cs
@@ -61,13 +61,13 @@ namespace WarsztatApp
                 sqlCommand.Parameters["@kodPocz"].Value = txtKodPocztowy.Text;
                
                 sqlCommand.ExecuteNonQuery();
-                MessageBox.Show("użytkownik dodany pomyślnie");
+                wyswietlDaneKlientow();
+                MessageBox.Show("klient dodany pomyślnie");
             }
             catch (SqlException)
             {
                 MessageBox.Show("Błędne dane", "Bląd", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                //Application.Restart();
-                //Environment.Exit(0);
+                
             }
 
             finally
@@ -93,15 +93,10 @@ namespace WarsztatApp
 
         private void button4_Click(object sender, EventArgs e)//wyświetlanie
         {
-           
             try
             {
                 sqlConnection.Open();
-                string Query = "SELECT * from widok_klient;";
-                sqlDAdapter = new SqlDataAdapter(Query, sqlConnection);
-                DataTable dtb1 = new DataTable();
-                sqlDAdapter.Fill(dtb1);
-                dataGridView1.DataSource = dtb1;
+                wyswietlDaneKlientow();
             }
             catch
             {
@@ -111,14 +106,31 @@ namespace WarsztatApp
             {
                 sqlConnection.Close();
             }
+           
+            
             
         }
 
-        private void button1_Click(object sender, EventArgs e)//usuwanie
+
+        private void wyswietlDaneKlientow()
         {
             
+                
+                string Query = "SELECT * from widok_klient;";
+                sqlDAdapter = new SqlDataAdapter(Query, sqlConnection);
+                DataTable dtb1 = new DataTable();
+                sqlDAdapter.Fill(dtb1);
+                dataGridView1.DataSource = dtb1;
             
-            int id = (int)dataGridView1.CurrentCell.Value;
+            
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)//usuwanie
+        {
+
+
+            int id = (int)dataGridView1.CurrentRow.Cells[0].Value;
             try
             {
                 sqlConnection.Open();
@@ -137,6 +149,14 @@ namespace WarsztatApp
             finally
             {
                 sqlConnection.Close();
+                txtImie.Text = "";
+                txtNazwisko.Text = "";
+                txtTelefon.Text = "";
+                txtUlica.Text = "";
+                txtNumer.Text = "";
+                txtMiasto.Text = "";
+                txtWojewodztwo.Text = "";
+                txtKodPocztowy.Text = "";
             }
 
         }
@@ -145,6 +165,75 @@ namespace WarsztatApp
         {
             index = e.RowIndex;
             DataGridViewRow row = dataGridView1.Rows[index];
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)//edycja
+        {
+            try
+            {
+                sqlConnection.Open();
+                int id = (int)dataGridView1.CurrentRow.Cells[0].Value;
+                string Query = "EXEC USP_EdytujDaneKlienta @id,@imie,@nazw,@tel,@ulica,@nrUlicy,@miasto,@wojew,@kodPocz";
+
+                sqlCommand = new SqlCommand
+                    (Query, sqlConnection);
+
+                sqlCommand.Parameters.Add("@id", SqlDbType.Int);
+                sqlCommand.Parameters.Add("@imie", SqlDbType.NVarChar);
+                sqlCommand.Parameters.Add("@nazw", SqlDbType.NVarChar);
+                sqlCommand.Parameters.Add("@tel", SqlDbType.NVarChar);
+                sqlCommand.Parameters.Add("@ulica", SqlDbType.NVarChar);
+                sqlCommand.Parameters.Add("@nrUlicy", SqlDbType.NVarChar);
+                sqlCommand.Parameters.Add("@miasto", SqlDbType.NVarChar);
+                sqlCommand.Parameters.Add("@wojew", SqlDbType.NVarChar);
+                sqlCommand.Parameters.Add("@kodPocz", SqlDbType.NVarChar);
+
+                sqlCommand.Parameters["@id"].Value = id.ToString();
+                sqlCommand.Parameters["@imie"].Value = txtImie.Text;
+                sqlCommand.Parameters["@nazw"].Value = txtNazwisko.Text;
+                sqlCommand.Parameters["@tel"].Value = txtTelefon.Text;
+                sqlCommand.Parameters["@ulica"].Value = txtUlica.Text;
+                sqlCommand.Parameters["@NrUlicy"].Value = txtNumer.Text;
+                sqlCommand.Parameters["@miasto"].Value = txtMiasto.Text;
+                sqlCommand.Parameters["@wojew"].Value = txtWojewodztwo.Text;
+                sqlCommand.Parameters["@kodPocz"].Value = txtKodPocztowy.Text;
+
+                sqlCommand.ExecuteNonQuery();
+                wyswietlDaneKlientow();
+                MessageBox.Show("klient edytowany pomyślnie");
+            }
+            catch (SqlException)
+            {
+                MessageBox.Show("Błędne dane", "Bląd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                
+            }
+
+            finally
+            {
+                sqlConnection.Close();
+                txtImie.Text = "";
+                txtNazwisko.Text = "";
+                txtTelefon.Text = "";
+                txtUlica.Text = "";
+                txtNumer.Text = "";
+                txtMiasto.Text = "";
+                txtWojewodztwo.Text = "";
+                txtKodPocztowy.Text = "";
+            }
+
+        }
+
+        private void dataGridView1_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            txtImie.Text=dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            txtNazwisko.Text=dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            txtTelefon.Text=dataGridView1.CurrentRow.Cells[3].Value.ToString();
+            txtUlica.Text=dataGridView1.CurrentRow.Cells[4].Value.ToString();
+            txtNumer.Text=dataGridView1.CurrentRow.Cells[5].Value.ToString();
+            txtMiasto.Text=dataGridView1.CurrentRow.Cells[6].Value.ToString();
+            txtWojewodztwo.Text=dataGridView1.CurrentRow.Cells[7].Value.ToString();
+            txtKodPocztowy.Text=dataGridView1.CurrentRow.Cells[8].Value.ToString();
         }
     }
 }
