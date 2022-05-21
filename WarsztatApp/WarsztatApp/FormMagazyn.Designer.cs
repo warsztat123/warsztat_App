488f69a6044fc3548c96c0e61200994dec60ac42
Adam Dziakiewicz <adam.dziakiewicz@outlook.com>
2022-05-21 21:54:46 +0200

    comboBox w magazynie, pobranie listy kategorii, dodawanie czesci samochodowych do DB

--- WarsztatApp/WarsztatApp/FormMagazyn.Designer.cs
+++ WarsztatApp/WarsztatApp/FormMagazyn.Designer.cs
@@ -28,35 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label6 = new System.Windows.Forms.Label();
+            this.Kategoria = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.txtIlosc = new System.Windows.Forms.Label();
-            this.txtCena = new System.Windows.Forms.Label();
-            this.txtNazwa = new System.Windows.Forms.Label();
-            this.button4 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.wyswietl = new System.Windows.Forms.Button();
             this.textBox6 = new System.Windows.Forms.TextBox();
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtIlosc = new System.Windows.Forms.TextBox();
+            this.txtCena = new System.Windows.Forms.TextBox();
+            this.txtNazwa = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.dodaj = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // label6
+            // Kategoria
             // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label6.Location = new System.Drawing.Point(12, 290);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(64, 25);
-            this.label6.TabIndex = 61;
-            this.label6.Text = "label6";
+            this.Kategoria.AutoSize = true;
+            this.Kategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Kategoria.Location = new System.Drawing.Point(7, 418);
+            this.Kategoria.Name = "Kategoria";
+            this.Kategoria.Size = new System.Drawing.Size(96, 25);
+            this.Kategoria.TabIndex = 61;
+            this.Kategoria.Text = "Kategoria";
             // 
             // label5
             // 
@@ -78,45 +79,46 @@
             this.label4.TabIndex = 59;
             this.label4.Text = "label4";
             // 
-            // txtIlosc
+            // label3
             // 
-            this.txtIlosc.AutoSize = true;
-            this.txtIlosc.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.txtIlosc.Location = new System.Drawing.Point(12, 129);
-            this.txtIlosc.Name = "txtIlosc";
-            this.txtIlosc.Size = new System.Drawing.Size(52, 25);
-            this.txtIlosc.TabIndex = 58;
-            this.txtIlosc.Text = "Ilość";
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(12, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 25);
+            this.label3.TabIndex = 58;
+            this.label3.Text = "Ilość";
             // 
-            // txtCena
+            // label2
             // 
-            this.txtCena.AutoSize = true;
-            this.txtCena.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.txtCena.Location = new System.Drawing.Point(12, 70);
-            this.txtCena.Name = "txtCena";
-            this.txtCena.Size = new System.Drawing.Size(60, 25);
-            this.txtCena.TabIndex = 57;
-            this.txtCena.Text = "Cena";
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(12, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 25);
+            this.label2.TabIndex = 57;
+            this.label2.Text = "Cena";
             // 
-            // txtNazwa
+            // label1
             // 
-            this.txtNazwa.AutoSize = true;
-            this.txtNazwa.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.txtNazwa.Location = new System.Drawing.Point(12, 16);
-            this.txtNazwa.Name = "txtNazwa";
-            this.txtNazwa.Size = new System.Drawing.Size(72, 25);
-            this.txtNazwa.TabIndex = 56;
-            this.txtNazwa.Text = "Nazwa";
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 25);
+            this.label1.TabIndex = 56;
+            this.label1.Text = "Nazwa";
             // 
-            // button4
+            // wyswietl
             // 
-            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button4.Location = new System.Drawing.Point(12, 484);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(351, 59);
-            this.button4.TabIndex = 55;
-            this.button4.Text = "Wyświetl";
-            this.button4.UseVisualStyleBackColor = true;
+            this.wyswietl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.wyswietl.Location = new System.Drawing.Point(12, 484);
+            this.wyswietl.Name = "wyswietl";
+            this.wyswietl.Size = new System.Drawing.Size(351, 59);
+            this.wyswietl.TabIndex = 55;
+            this.wyswietl.Text = "Wyświetl";
+            this.wyswietl.UseVisualStyleBackColor = true;
+            this.wyswietl.Click += new System.EventHandler(this.wyswietl_Click);
             // 
             // textBox6
             // 
@@ -142,29 +144,29 @@
             this.textBox4.Size = new System.Drawing.Size(212, 30);
             this.textBox4.TabIndex = 52;
             // 
-            // textBox3
+            // txtIlosc
             // 
-            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.textBox3.Location = new System.Drawing.Point(151, 126);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(212, 30);
-            this.textBox3.TabIndex = 51;
+            this.txtIlosc.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtIlosc.Location = new System.Drawing.Point(151, 126);
+            this.txtIlosc.Name = "txtIlosc";
+            this.txtIlosc.Size = new System.Drawing.Size(212, 30);
+            this.txtIlosc.TabIndex = 51;
             // 
-            // textBox2
+            // txtCena
             // 
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.textBox2.Location = new System.Drawing.Point(151, 67);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(212, 30);
-            this.textBox2.TabIndex = 50;
+            this.txtCena.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtCena.Location = new System.Drawing.Point(151, 67);
+            this.txtCena.Name = "txtCena";
+            this.txtCena.Size = new System.Drawing.Size(212, 30);
+            this.txtCena.TabIndex = 50;
             // 
-            // textBox1
+            // txtNazwa
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.textBox1.Location = new System.Drawing.Point(151, 13);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(212, 30);
-            this.textBox1.TabIndex = 49;
+            this.txtNazwa.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtNazwa.Location = new System.Drawing.Point(151, 13);
+            this.txtNazwa.Name = "txtNazwa";
+            this.txtNazwa.Size = new System.Drawing.Size(212, 30);
+            this.txtNazwa.TabIndex = 49;
             // 
             // button1
             // 
@@ -186,15 +188,16 @@
             this.button2.Text = "Modyfikuj";
             this.button2.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // dodaj
             // 
-            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button3.Location = new System.Drawing.Point(12, 679);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(351, 59);
-            this.button3.TabIndex = 46;
-            this.button3.Text = "Dodaj";
-            this.button3.UseVisualStyleBackColor = true;
+            this.dodaj.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.dodaj.Location = new System.Drawing.Point(12, 679);
+            this.dodaj.Name = "dodaj";
+            this.dodaj.Size = new System.Drawing.Size(351, 59);
+            this.dodaj.TabIndex = 46;
+            this.dodaj.Text = "Dodaj";
+            this.dodaj.UseVisualStyleBackColor = true;
+            this.dodaj.Click += new System.EventHandler(this.dodaj_Click);
             // 
             // dataGridView1
             // 
@@ -209,27 +212,37 @@
             this.dataGridView1.Size = new System.Drawing.Size(805, 725);
             this.dataGridView1.TabIndex = 45;
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(110, 418);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(253, 24);
+            this.comboBox1.TabIndex = 62;
+            this.comboBox1.SelectionChangeCommitted += new System.EventHandler(this.comboBox1_SelectionChangeCommitted);
+            // 
             // FormMagazyn
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1200, 750);
-            this.Controls.Add(this.label6);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.Kategoria);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.txtIlosc);
-            this.Controls.Add(this.txtCena);
-            this.Controls.Add(this.txtNazwa);
-            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.wyswietl);
             this.Controls.Add(this.textBox6);
             this.Controls.Add(this.textBox5);
             this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtIlosc);
+            this.Controls.Add(this.txtCena);
+            this.Controls.Add(this.txtNazwa);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dodaj);
             this.Controls.Add(this.dataGridView1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FormMagazyn";
@@ -244,22 +257,23 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label Kategoria;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label txtIlosc;
-        private System.Windows.Forms.Label txtCena;
-        private System.Windows.Forms.Label txtNazwa;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button wyswietl;
         private System.Windows.Forms.TextBox textBox6;
         private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtIlosc;
+        private System.Windows.Forms.TextBox txtCena;
+        private System.Windows.Forms.TextBox txtNazwa;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button dodaj;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
--- WarsztatApp/WarsztatApp/FormMagazyn.cs
+++ WarsztatApp/WarsztatApp/FormMagazyn.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,119 @@ namespace WarsztatApp
 {
     public partial class FormMagazyn : Form
     {
+        static string ConnectionString = "Data Source=LAPTOP-442E11CB\\SQLEXPRESS;"
+                                                 + "Initial Catalog=Warsztat;"
+                                                 + "Integrated Security=True";
+        SqlConnection sqlConnection = new SqlConnection(ConnectionString);
+        SqlCommand sqlCommand;
+        SqlDataAdapter sqlDAdapter = new SqlDataAdapter();
+        SqlDataReader sqlDReader;
+        List<string> str1 = new List<string>();
+        private string s = "";
         public FormMagazyn()
         {
             InitializeComponent();
+            pobierzListeKategorii();
+        }
+
+        private void dodaj_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                sqlConnection.Open();
+
+                string Query = "insert into CzescSamochodowa(Nazwa,Cena,Ilosc,Kategoria_ID) values(@nazwa,@cena,@ilosc,@kategoria_id);";
+                sqlCommand = new SqlCommand
+                    (Query, sqlConnection);
+
+                sqlCommand.Parameters.Add("@nazwa", SqlDbType.NVarChar);
+                sqlCommand.Parameters.Add("@cena", SqlDbType.Decimal);
+                sqlCommand.Parameters.Add("@ilosc", SqlDbType.Int);
+                sqlCommand.Parameters.Add("@kategoria_id", SqlDbType.Int);
+
+                sqlCommand.Parameters["@nazwa"].Value = txtNazwa.Text;
+                sqlCommand.Parameters["@cena"].Value = txtCena.Text;
+                sqlCommand.Parameters["@ilosc"].Value = txtIlosc.Text;
+                sqlCommand.Parameters["@kategoria_id"].Value = s;
+
+                sqlCommand.ExecuteNonQuery();
+                MessageBox.Show("część dodana pomyślnie");
+            }
+            catch
+            {
+                MessageBox.Show("Błędne dane", "Bląd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+            
+        }
+
+        private void pobierzListeKategorii()
+        {
+            
+            sqlConnection.Open();
+            string Query = "SELECT * from KategoriaCzesci;";
+            sqlCommand = new SqlCommand
+                    (Query, sqlConnection);
+            sqlDReader = sqlCommand.ExecuteReader();
+            while (sqlDReader.Read())
+            {
+                str1.Add(sqlDReader.GetValue(2).ToString());
+            }
+            foreach (string p in str1)
+            {
+                comboBox1.Items.Add(p);
+            }
+            sqlDReader.Close();
+            sqlConnection.Close();
+            str1.Clear();
+
+        }
+
+        private void comboBox1_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+           
+            
+            try
+            {
+                
+                sqlConnection.Open();
+
+                string Query = "SELECT ParentKategoriaCzesci_ID from KategoriaCzesci where Nazwa=@kategoria;";
+                sqlCommand = new SqlCommand
+                    (Query, sqlConnection);
+
+                sqlCommand.Parameters.Add("@kategoria", SqlDbType.NVarChar);
+                sqlCommand.Parameters["@kategoria"].Value = comboBox1.GetItemText(comboBox1.SelectedItem);
+                sqlDReader = sqlCommand.ExecuteReader();
+                
+                while (sqlDReader.Read())
+                {
+                    s = sqlDReader.GetValue(0).ToString();
+                }
+
+
+            }
+            catch
+            {
+                MessageBox.Show("Błąd", "Bląd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                sqlDReader.Close();
+                sqlConnection.Close();
+            }
+            
+        }
+
+        private void wyswietl_Click(object sender, EventArgs e)
+        {
+            
         }
     }
+
+   
 }
