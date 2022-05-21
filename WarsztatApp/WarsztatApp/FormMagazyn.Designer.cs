b8fd51c4e48c3354fcc52317a12a6394cf4a584c
Adam Dziakiewicz <adam.dziakiewicz@outlook.com>
2022-05-21 19:08:41 +0200

    label nazwa, cena, ilosc

--- WarsztatApp/WarsztatApp/FormMagazyn.Designer.cs
+++ WarsztatApp/WarsztatApp/FormMagazyn.Designer.cs
@@ -31,9 +31,9 @@
             this.label6 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
+            this.txtIlosc = new System.Windows.Forms.Label();
+            this.txtCena = new System.Windows.Forms.Label();
+            this.txtNazwa = new System.Windows.Forms.Label();
             this.button4 = new System.Windows.Forms.Button();
             this.textBox6 = new System.Windows.Forms.TextBox();
             this.textBox5 = new System.Windows.Forms.TextBox();
@@ -78,35 +78,35 @@
             this.label4.TabIndex = 59;
             this.label4.Text = "label4";
             // 
-            // label3
+            // txtIlosc
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label3.Location = new System.Drawing.Point(12, 129);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(64, 25);
-            this.label3.TabIndex = 58;
-            this.label3.Text = "label3";
+            this.txtIlosc.AutoSize = true;
+            this.txtIlosc.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtIlosc.Location = new System.Drawing.Point(12, 129);
+            this.txtIlosc.Name = "txtIlosc";
+            this.txtIlosc.Size = new System.Drawing.Size(52, 25);
+            this.txtIlosc.TabIndex = 58;
+            this.txtIlosc.Text = "Ilość";
             // 
-            // label2
+            // txtCena
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label2.Location = new System.Drawing.Point(12, 70);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(64, 25);
-            this.label2.TabIndex = 57;
-            this.label2.Text = "label2";
+            this.txtCena.AutoSize = true;
+            this.txtCena.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtCena.Location = new System.Drawing.Point(12, 70);
+            this.txtCena.Name = "txtCena";
+            this.txtCena.Size = new System.Drawing.Size(60, 25);
+            this.txtCena.TabIndex = 57;
+            this.txtCena.Text = "Cena";
             // 
-            // label1
+            // txtNazwa
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label1.Location = new System.Drawing.Point(12, 16);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(64, 25);
-            this.label1.TabIndex = 56;
-            this.label1.Text = "label1";
+            this.txtNazwa.AutoSize = true;
+            this.txtNazwa.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtNazwa.Location = new System.Drawing.Point(12, 16);
+            this.txtNazwa.Name = "txtNazwa";
+            this.txtNazwa.Size = new System.Drawing.Size(72, 25);
+            this.txtNazwa.TabIndex = 56;
+            this.txtNazwa.Text = "Nazwa";
             // 
             // button4
             // 
@@ -217,9 +217,9 @@
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtIlosc);
+            this.Controls.Add(this.txtCena);
+            this.Controls.Add(this.txtNazwa);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.textBox6);
             this.Controls.Add(this.textBox5);
@@ -247,9 +247,9 @@
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label txtIlosc;
+        private System.Windows.Forms.Label txtCena;
+        private System.Windows.Forms.Label txtNazwa;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.TextBox textBox6;
         private System.Windows.Forms.TextBox textBox5;
