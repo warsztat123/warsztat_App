9f3d8b51739734874679dccc4f8f297e0c57dadf
Arek Dabrowski <arek200299@op.pl>
2022-05-29 16:19:38 +0200

    dodalem Form ktory dodaje czesci do wybranego zlecenia - jeszcze nieskonczone

---
+++ WarsztatApp/WarsztatApp/FormDodaniaCzesciDoZlecenia.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace WarsztatApp
+{
+    partial class FormDodaniaCzesciDoZlecenia
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.buttonWycofaj = new System.Windows.Forms.Button();
+            this.buttonZatwierdz = new System.Windows.Forms.Button();
+            this.labelKategoria = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.buttonUsun = new System.Windows.Forms.Button();
+            this.buttonDodaj = new System.Windows.Forms.Button();
+            this.labelIlosc = new System.Windows.Forms.Label();
+            this.numericUpDownIlosc = new System.Windows.Forms.NumericUpDown();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownIlosc)).BeginInit();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(400, 553);
+            this.panel1.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(400, 553);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.buttonWycofaj);
+            this.panel2.Controls.Add(this.buttonZatwierdz);
+            this.panel2.Controls.Add(this.labelKategoria);
+            this.panel2.Controls.Add(this.comboBox1);
+            this.panel2.Controls.Add(this.buttonUsun);
+            this.panel2.Controls.Add(this.buttonDodaj);
+            this.panel2.Controls.Add(this.labelIlosc);
+            this.panel2.Controls.Add(this.numericUpDownIlosc);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(400, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(182, 553);
+            this.panel2.TabIndex = 3;
+            // 
+            // buttonWycofaj
+            // 
+            this.buttonWycofaj.BackColor = System.Drawing.Color.Red;
+            this.buttonWycofaj.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonWycofaj.ForeColor = System.Drawing.Color.White;
+            this.buttonWycofaj.Location = new System.Drawing.Point(8, 447);
+            this.buttonWycofaj.Margin = new System.Windows.Forms.Padding(5);
+            this.buttonWycofaj.Name = "buttonWycofaj";
+            this.buttonWycofaj.Size = new System.Drawing.Size(166, 41);
+            this.buttonWycofaj.TabIndex = 7;
+            this.buttonWycofaj.Text = "Wycofaj";
+            this.buttonWycofaj.UseVisualStyleBackColor = false;
+            this.buttonWycofaj.Click += new System.EventHandler(this.buttonWycofaj_Click);
+            // 
+            // buttonZatwierdz
+            // 
+            this.buttonZatwierdz.BackColor = System.Drawing.Color.Lime;
+            this.buttonZatwierdz.Enabled = false;
+            this.buttonZatwierdz.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonZatwierdz.Location = new System.Drawing.Point(8, 498);
+            this.buttonZatwierdz.Margin = new System.Windows.Forms.Padding(5);
+            this.buttonZatwierdz.Name = "buttonZatwierdz";
+            this.buttonZatwierdz.Size = new System.Drawing.Size(166, 41);
+            this.buttonZatwierdz.TabIndex = 6;
+            this.buttonZatwierdz.Text = "Zatwierdz";
+            this.buttonZatwierdz.UseVisualStyleBackColor = false;
+            this.buttonZatwierdz.Click += new System.EventHandler(this.buttonZatwierdz_Click);
+            // 
+            // labelKategoria
+            // 
+            this.labelKategoria.AutoSize = true;
+            this.labelKategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelKategoria.Location = new System.Drawing.Point(6, 29);
+            this.labelKategoria.Name = "labelKategoria";
+            this.labelKategoria.Size = new System.Drawing.Size(133, 29);
+            this.labelKategoria.TabIndex = 5;
+            this.labelKategoria.Text = "Kategoria:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(11, 61);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(163, 26);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedValueChanged += new System.EventHandler(this.comboBox1_SelectedValueChanged);
+            // 
+            // buttonUsun
+            // 
+            this.buttonUsun.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonUsun.Location = new System.Drawing.Point(8, 272);
+            this.buttonUsun.Margin = new System.Windows.Forms.Padding(5);
+            this.buttonUsun.Name = "buttonUsun";
+            this.buttonUsun.Size = new System.Drawing.Size(166, 41);
+            this.buttonUsun.TabIndex = 3;
+            this.buttonUsun.Text = "Usuń";
+            this.buttonUsun.UseVisualStyleBackColor = true;
+            this.buttonUsun.Click += new System.EventHandler(this.buttonUsun_Click);
+            // 
+            // buttonDodaj
+            // 
+            this.buttonDodaj.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonDodaj.Location = new System.Drawing.Point(8, 221);
+            this.buttonDodaj.Margin = new System.Windows.Forms.Padding(5);
+            this.buttonDodaj.Name = "buttonDodaj";
+            this.buttonDodaj.Size = new System.Drawing.Size(166, 41);
+            this.buttonDodaj.TabIndex = 2;
+            this.buttonDodaj.Text = "Dodaj";
+            this.buttonDodaj.UseVisualStyleBackColor = true;
+            this.buttonDodaj.Click += new System.EventHandler(this.buttonDodaj_Click);
+            // 
+            // labelIlosc
+            // 
+            this.labelIlosc.AutoSize = true;
+            this.labelIlosc.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelIlosc.Location = new System.Drawing.Point(6, 156);
+            this.labelIlosc.Name = "labelIlosc";
+            this.labelIlosc.Size = new System.Drawing.Size(75, 29);
+            this.labelIlosc.TabIndex = 1;
+            this.labelIlosc.Text = "Ilość:";
+            // 
+            // numericUpDownIlosc
+            // 
+            this.numericUpDownIlosc.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.numericUpDownIlosc.Location = new System.Drawing.Point(104, 154);
+            this.numericUpDownIlosc.Name = "numericUpDownIlosc";
+            this.numericUpDownIlosc.Size = new System.Drawing.Size(72, 34);
+            this.numericUpDownIlosc.TabIndex = 0;
+            this.numericUpDownIlosc.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.dataGridView2);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel3.Location = new System.Drawing.Point(582, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(400, 553);
+            this.panel3.TabIndex = 3;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView2.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(400, 553);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // FormDodaniaCzesciDoZlecenia
+            // 
+            this.AcceptButton = this.buttonZatwierdz;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.buttonWycofaj;
+            this.ClientSize = new System.Drawing.Size(982, 553);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "FormDodaniaCzesciDoZlecenia";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FormDodaniaCzesciDoZlecenia";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownIlosc)).EndInit();
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button buttonDodaj;
+        private System.Windows.Forms.Label labelIlosc;
+        private System.Windows.Forms.NumericUpDown numericUpDownIlosc;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button buttonUsun;
+        private System.Windows.Forms.Button buttonWycofaj;
+        private System.Windows.Forms.Button buttonZatwierdz;
+        private System.Windows.Forms.Label labelKategoria;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
---
+++ WarsztatApp/WarsztatApp/FormDodaniaCzesciDoZlecenia.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WarsztatApp
+{
+    public partial class FormDodaniaCzesciDoZlecenia : Form
+    {
+        SqlConnection sqlConnection = new SqlConnection();
+        SqlCommand sqlCommand = new SqlCommand();
+        DataTable sqlDTable = new DataTable();
+        String sqlQuery;
+        SqlDataAdapter sqlDAdapter = new SqlDataAdapter();
+        DataSet sqlDSet = new DataSet();
+        SqlDataReader sqlDReader;
+
+        List<string> str1 = new List<string>();
+        public int id_row;
+        DataGridViewRow row;
+
+        int currentIdRowZlecenia;
+        FormZlecenia oknoZlec;
+
+        public FormDodaniaCzesciDoZlecenia(FormZlecenia KtoOknoZlecenia)
+        {
+            InitializeComponent();
+            oknoZlec = KtoOknoZlecenia;
+            pobierzIdZlecenia();
+
+            numericUpDownIlosc.Enabled = false;
+            buttonDodaj.Enabled = false;
+            buttonUsun.Enabled = false;
+
+            sqlConnection.ConnectionString = "Data Source=WINDOWS-D2AGQGT\\SQLEXPRESS;"
+                                                + "Initial Catalog=Warsztat;"
+                                                + "Integrated Security = true;";
+            startTransakcji();
+            pobierzListeKategorii();
+            
+        }
+
+
+        private void comboBox1_SelectedValueChanged(object sender, EventArgs e)
+        {
+            wyszukajDostepneCzesciWedlugKategorii();
+        }
+
+
+        private void pobierzListeKategorii()
+        {
+            //sqlConnection.Open();
+            string Query = "SELECT * from KategoriaCzesci;";
+            sqlCommand = new SqlCommand(Query, sqlConnection);
+            sqlDReader = sqlCommand.ExecuteReader();
+            while (sqlDReader.Read())
+            {
+                str1.Add(sqlDReader.GetValue(2).ToString());
+            }
+            foreach (string p in str1)
+            {
+                comboBox1.Items.Add(p);
+            }
+            sqlDReader.Close();
+            //sqlConnection.Close();
+            str1.Clear();
+        }
+
+        private void buttonDodaj_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string sqlQuery2 = "EXEC USP_DodajCzesciDoZlecenia @IDCzesci, @IDZlecenia, @Ilosc;";
+                SqlCommand sqlCommand2 = new SqlCommand(sqlQuery2, sqlConnection);
+                DataTable sqlDTableTemp2 = new DataTable();
+                sqlCommand2.Parameters.Add("@IDCzesci", SqlDbType.Int);
+                sqlCommand2.Parameters.Add("@IDZlecenia", SqlDbType.Int);
+                sqlCommand2.Parameters.Add("@Ilosc", SqlDbType.Int);
+                sqlCommand2.Parameters["@IDCzesci"].Value = id_row;
+                sqlCommand2.Parameters["@IDZlecenia"].Value = currentIdRowZlecenia;
+                sqlCommand2.Parameters["@Ilosc"].Value = numericUpDownIlosc.Value;
+                sqlDReader = sqlCommand2.ExecuteReader();
+                sqlDTableTemp2.Load(sqlDReader);
+                sqlDReader.Close();
+
+                string sqlQuery3 = "SELECT Zlecenie_ID, CzescSamochodowa_ID, Ilosc, CenaZaSzt, CalkowityKoszt " +
+                                    "FROM Zlecenie_CzescSamochodowa";
+                SqlCommand sqlCommand3 = new SqlCommand(sqlQuery3, sqlConnection);
+                DataTable sqlDTableTemp3 = new DataTable();
+                sqlDReader = sqlCommand3.ExecuteReader();
+                sqlDTableTemp3.Load(sqlDReader);
+                sqlDReader.Close();
+                dataGridView2.DataSource = sqlDTableTemp3;
+
+                wyszukajDostepneCzesciWedlugKategorii();
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("Nie ma już produktu na stanie", "Uwaga", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            
+
+        }
+
+        private void buttonUsun_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonZatwierdz_Click(object sender, EventArgs e)
+        {
+            string Query = "COMMIT TRAN;";
+            sqlCommand = new SqlCommand(Query, sqlConnection);
+            sqlDReader = sqlCommand.ExecuteReader();
+            sqlDReader.Close();
+            sqlConnection.Close();
+            Close();
+        }
+
+        private void buttonWycofaj_Click(object sender, EventArgs e)
+        {
+            string Query = "ROLLBACK TRAN;";
+            sqlCommand = new SqlCommand(Query, sqlConnection);
+            sqlDReader = sqlCommand.ExecuteReader();
+            sqlDReader.Close();
+            sqlConnection.Close();
+            Close();
+        }
+
+        private void startTransakcji()
+        {
+            sqlConnection.Open();
+            string Query = "BEGIN TRAN;";
+            sqlCommand = new SqlCommand(Query, sqlConnection);
+            sqlDReader = sqlCommand.ExecuteReader();
+            sqlDReader.Close();
+        }
+
+        private void pobierzIdZlecenia()
+        {
+            currentIdRowZlecenia = oknoZlec.id_row;
+            //labelIlosc.Text = currentIdRowZlecenia.ToString();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                if (e.RowIndex >= 0)
+                {
+                    buttonDodaj.Enabled = true;
+                    numericUpDownIlosc.Enabled = true;
+                    row = this.dataGridView1.Rows[e.RowIndex];
+                    id_row = (int)row.Cells["CzescSamochodowa_ID"].Value;
+                }
+                else
+                {
+                    buttonDodaj.Enabled = false;
+                    numericUpDownIlosc.Enabled = false;
+                }
+                
+                sqlCommand.CommandText = "SELECT Ilosc FROM CzescSamochodowa WHERE CzescSamochodowa_ID = @id";
+                sqlCommand.Parameters.Add("@id", SqlDbType.Int);
+                sqlCommand.Parameters["@id"].Value = id_row;
+                sqlDReader = sqlCommand.ExecuteReader();
+                sqlDReader.Read();
+                numericUpDownIlosc.Maximum = sqlDReader.GetInt32(0);
+                if (numericUpDownIlosc.Maximum == 0)
+                {
+                    buttonDodaj.Enabled = false;
+                    sqlDReader.Close();
+                }
+                else
+                {
+                    numericUpDownIlosc.Value = 1;
+                }
+                sqlCommand.Parameters.Clear();
+                sqlDReader.Close();
+            }
+            catch (Exception)
+            {
+                buttonDodaj.Enabled = false;
+                numericUpDownIlosc.Enabled = false;
+                MessageBox.Show("Błednie zaznaczony wiersz", "Bląd", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void wyszukajDostepneCzesciWedlugKategorii()
+        {
+            sqlQuery = "SELECT CzescSamochodowa_ID, Nazwa, Cena, Ilosc " +
+                       "FROM CzescSamochodowa " +
+                       "WHERE Kategoria_ID IN " +
+                                            "(SELECT KategoriaCzesci_ID " +
+                                            "FROM KategoriaCzesci " +
+                                            "WHERE Nazwa = @nazwaKat);";
+            SqlCommand sqlCommand = new SqlCommand(sqlQuery, sqlConnection);
+            DataTable sqlDTableTemp = new DataTable();
+            //sqlConnection.Open();
+            sqlCommand.Parameters.Add("@nazwaKat", SqlDbType.NVarChar);
+            sqlCommand.Parameters["@nazwaKat"].Value = comboBox1.SelectedItem.ToString();
+            sqlDReader = sqlCommand.ExecuteReader();
+            sqlDTableTemp.Load(sqlDReader);
+            sqlDReader.Close();
+            //sqlConnection.Close();
+            dataGridView1.DataSource = sqlDTableTemp;
+            str1.Clear();
+        }
+
+    }
+}
